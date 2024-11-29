87132b1799d0592fa4ff7af54524796a5630a48a
unknown <murkdr64@gmail.com>
2024-11-29 11:57:01 -0800

    dr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import Propsy from './propsy'
+"use client";
+import React from 'react';
+import Propsy from './propsy';
+import { useState } from 'react';
 
 const page = () => {
+  const [name, setName] = useState({
+    name: "",
+    favdish: "",
+    favcolour: ""
+  });
+
   return (
-    <div>
-
-    <Propsy favname={"Nischay😊"} favdish={"Dosaa😋"} favcolor={"Black🖤"}/>
-   
-
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-8">
+      {/* Passing data to Propsy component */}
+      <Propsy favname={name.name} favdish={name.favdish} favcolour={name.favcolour} />
+      
+      <button
+        className="bg-[#289ab1] text-white border-x-8 border-[#0f566e] border-y-4 px-6 py-3 rounded md:px-8 md:py-4 sm:px-4 sm:py-2"
+        onClick={() => {
+          setName({
+            name: "Name: Shumaila",
+            favdish: "Favourite Dish: Biryani",
+            favcolour: "Favourite Colour: Black"
+          });
+        }}
+      >
+        Click here 😊
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- src/app/propsy.tsx
+++ src/app/propsy.tsx
@@ -2,14 +2,14 @@
 
 const Propsy = (props :any) => {
     return (
-     <div className="flex justify-center items-center h-screen">
-     <div className= "text-[#5f1c4e] text-xl font-serif bg-pink-300 h-60 w-60 text-center px-10 py-9 border-x-4 border-pink-600 border-y-4 ">
-     
-        <h1>Favourite Name: {props.favname}</h1>
-        <h2>Favourite Dish: {props.favdish}</h2>
-        <h2>Favourite Colour: {props.favcolor}</h2>
+    //  <div className="flex justify-center items-center h-screen">
+    //  <div className= "text-[#5f1c4e] text-xl font-serif bg-pink-300 h-60 w-60 text-center px-10 py-9 border-x-4 border-pink-600 border-y-4 ">
+     <div>
+       <h1 className="font-sans text-xl text-[#227aa3] font-semibold">{props.favname}</h1>
+       <h2 className="font-sans text-xl text-[#227aa3] font-semibold">{props.favcolour}</h2> 
+       <h2 className="font-sans text-xl text-[#227aa3] font-semibold">{props.favdish}</h2> 
       </div>
-      </div>
+      // </div>
     );
   };
   
